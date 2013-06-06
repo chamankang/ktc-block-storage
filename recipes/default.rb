d469b94dc89faaa08a5f6848517f0f71f452180f
Robert <taeil.choi@kt.com>
2013-06-06 13:18:21 +0900

    add api recipe for adding glance config to cinder.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,55 +17,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "cinder::cinder-volume"
 
-cookbook_file node['cinder']['services']['volume']['register_script'] do
-  source "crm_register"
-  owner "root"
-  group "root"
-  mode 0755
-  action :create_if_missing
-end
-
-cookbook_file node['cinder']['services']['volume']['delete_script'] do
-  source "crm_delete"
-  owner "root"
-  group "root"
-  mode 0755
-  action :create_if_missing
-  notifies :create, "template[/etc/cinder/rootwrap.d/volume.filters]", :immediately
-end
-
-template "/etc/cinder/rootwrap.d/volume.filters" do
-  source "volume_filters.erb"
-  owner "root"
-  group "root"
-  mode 0755
-  action :nothing
-  notifies :create, "cookbook_file[/usr/share/pyshared/cinder/volume/iscsi.py]", :immediately
-end
-
-cookbook_file "/usr/share/pyshared/cinder/volume/iscsi.py" do
-  source "iscsi.py"
-  owner "root"
-  group "root"
-  mode 0755
-  action :nothing
-  notifies :create, "ruby_block[add-host-myip-to-cinder-volume]", :immediately
-end
-
-ruby_block 'add-host-myip-to-cinder-volume' do
-  block do
-    myip_line = "myip = #{node['cinder']['services']['volume']['myip']}"
-    host_line = "host = #{node['cinder']['services']['volume']['host']}"
-
-    conf_file = Chef::Util::FileEdit.new("/etc/cinder/cinder.conf")
-    conf_file.insert_line_after_match(/iscsi_ip_address.*/, myip_line)
-    conf_file.insert_line_after_match(/iscsi_ip_address.*/, host_line)
-    conf_file.write_file
-  
-    system('touch /var/lock/.cinder_conf_edit_done')
-  end
-  action :nothing
-  not_if "test -f /var/lock/.cinder_conf_edit_done"
-end
+include_recipe "ktc-cinder::volume"
