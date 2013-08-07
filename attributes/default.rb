70fad2eff797aea4e5aafacca89a7ec1dc3ac098
Wil Reichert <wil.reichert@kt.com>
2013-08-07 11:30:25 +0900

    cleanup old files

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,1 @@
 # Set vip for cinder-volume service and iscsi connection
-default['cinder']['services']['volume']['myip'] = ""
-default['cinder']['services']['volume']['host'] = "snode"
-
-default['cinder']['services']['volume']['crm_cmd'] = "/usr/sbin/crm"
-default['cinder']['services']['volume']['collect_tids_cmd'] = "/usr/sbin/collect_tids"
-
