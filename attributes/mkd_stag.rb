1c297fa2542bcf35e5e154ba7335312e1326ef89
Jesse Nelson <spheromak@gmail.com>
2013-10-30 00:09:20 -0700

    update for disk side  on nfs shares

--- attributes/mkd_stag.rb
+++ attributes/mkd_stag.rb
@@ -10,12 +10,12 @@ default["openstack"]["block-storage"]["enabled_backends"] = {
 }
 default["openstack"]["block-storage"]["nexenta"]["nfs_shares"] = [
   {
-    "server_ip" => "snode1",
+    "server_ip" => "snode1-disk",
     "mount_point" => "/volumes/mkd-stag-snode2-zpool1/cinder1",
     "management_url" => "http://admin:nexenta@snode1:2000/"
   },
   {
-    "server_ip" => "snode2",
+    "server_ip" => "snode2-disk",
     "mount_point" => "/volumes/mkd-stag-snode2-zpool1/cinder1",
     "management_url" => "http://admin:nexenta@snode2:2000/"
   }
