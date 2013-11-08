ad828aa991ff8691f352500edb3258e9e197de0f
Sunhee Ahn <itskewpie@gmail.com>
2013-11-08 10:01:44 +0900

    add process monitoring(sensu check and collectd plugin)

--- recipes/default.rb
+++ recipes/default.rb
@@ -46,3 +46,22 @@ require 'chef/rewind'
 end
 
 include_recipe "openstack-block-storage::identity_registration"
+
+
+# process monitoring and sensu-check config
+processes = node['openstack']['block-storage']['api_processes']
+
+processes.each do |process|
+  sensu_check "check_process_#{process['name']}" do
+    command "check-procs.rb -c 10 -w 10 -C 1 -W 1 -p #{process['name']}"
+    handlers ["default"]
+    standalone true
+    interval 20
+  end 
+end
+
+collectd_processes "block-storage-api-processes" do
+  input processes
+  key "shortname"
+end
+
--- recipes/volume.rb
+++ recipes/volume.rb
@@ -47,3 +47,20 @@ end
 rewind :template => "/etc/tgt/targets.conf" do
   action :nothing
 end
+
+# process monitoring and sensu-check config
+processes = node['openstack']['block-storage']['volume_processes']
+
+processes.each do |process|
+  sensu_check "check_process_#{process['name']}" do
+    command "check-procs.rb -c 10 -w 10 -C 1 -W 1 -p #{process['name']}"
+    handlers ["default"]
+    standalone true
+    interval 20
+  end 
+end
+
+collectd_processes "block-storage-volume-processes" do
+  input processes
+  key "shortname"
+end
