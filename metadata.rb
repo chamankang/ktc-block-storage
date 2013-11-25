1f0e34f164c5ebc59b41347b5a3d9a4e5ac2eb48
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-11-25 13:58:00 +0900

    auto_version to 0.2.11

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "ktc-block-storage"
 maintainer        "Robert Choi"
 license           "Apache 2.0"
 description       "KTC-wrapper for block-storage cookbook"
-version '0.2.10'
+version '0.2.11'
 
 %w{ centos ubuntu }.each do |os|
   supports os
