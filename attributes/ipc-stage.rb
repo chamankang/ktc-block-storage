13266adbc56eb8e7d007def01ed1688195df33f0
chamankang <chaman.kang@kt.com>
2014-01-21 20:42:59 +0900

    update zone info for ipc-stage

--- attributes/ipc-stage.rb
+++ attributes/ipc-stage.rb
@@ -2,8 +2,6 @@ return unless chef_environment == "ipc-stage"
 
 include_attribute "ktc-block-storage::default"
 
-default["cinder_version"] = "2013.2.1.dev47.g430f0b9"
-
 default["openstack"]["block-storage"]["enabled_backends"] = {
   "nex-nfs1" => {
     "volume_driver" => "cinder.volume.drivers.nexenta.nfs.NexentaNfsDriver"
@@ -14,6 +12,25 @@ default["openstack"]["block-storage"]["nexenta"]["nfs_shares"] = [
   {
     "server_ip" => "10.210.2.23",
     "mount_point" => "/volumes/ipc-preprod-pool1/cinder1",
-    "management_url" => "http://admin:nexenta@10.9.242.240:2000/"
+    "management_url" => "http://admin:nexenta@10.9.242.240:2000/",
+    "zone_name" => "cheonan.prod.ktis"
+  },
+  {
+    "server_ip" => "10.210.2.23",
+    "mount_point" => "/volumes/ipc-preprod-pool1/cinder1",
+    "management_url" => "http://admin:nexenta@10.9.242.240:2000/",
+    "zone_name" => "cheonan.prod.dmz"
+  },
+  {
+    "server_ip" => "10.210.2.23",
+    "mount_point" => "/volumes/ipc-preprod-pool1/cinder1",
+    "management_url" => "http://admin:nexenta@10.9.242.240:2000/",
+    "zone_name" => "cheonan.dev.ktis"
+  },
+  {
+    "server_ip" => "10.210.2.23",
+    "mount_point" => "/volumes/ipc-preprod-pool1/cinder1",
+    "management_url" => "http://admin:nexenta@10.9.242.240:2000/",
+    "zone_name" => "cheonan.dev.dmz"
   }
 ]
