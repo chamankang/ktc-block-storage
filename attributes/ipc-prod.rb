f5e49e125771bb69557d62f65f648b08882774a3
chamankang <chaman.kang@kt.com>
2014-01-29 17:27:02 +0900

    fix ipc-prod nfs path

--- attributes/ipc-prod.rb
+++ attributes/ipc-prod.rb
@@ -11,25 +11,25 @@ default["openstack"]["block-storage"]["enabled_backends"] = {
 default["openstack"]["block-storage"]["nexenta"]["nfs_shares"] = [
   {
     "server_ip" => "10.220.88.119",
-    "mount_point" => "/pool1-t2spod1-ktisprod/cinder01",
+    "mount_point" => "/volumes/pool1-t2spod1-ktisprod/cinder01",
     "management_url" => "http://admin:xw6RQkpwE4@10.4.15.240:2000/",
     "zone_name" => "cheonan.prod.ktis"
   },
   {
     "server_ip" => "10.220.88.120",
-    "mount_point" => "/pool2-t2spod1-ktisprod/cinder02",
+    "mount_point" => "/volumes/pool2-t2spod1-ktisprod/cinder02",
     "management_url" => "http://admin:xw6RQkpwE4@10.4.15.241:2000/",
     "zone_name" => "cheonan.prod.ktis"
   },
   {
     "server_ip" => "10.220.88.121",
-    "mount_point" => "/pool3-t2spod1-ktisprod/cinder03",
+    "mount_point" => "/volumes/pool3-t2spod1-ktisprod/cinder03",
     "management_url" => "http://admin:xw6RQkpwE4@10.4.15.242:2000/",
     "zone_name" => "cheonan.prod.ktis"
   },
   {
     "server_ip" => "10.220.88.122",
-    "mount_point" => "/pool4-t2spod1-ktisprod/cinder04",
+    "mount_point" => "/volumes/pool4-t2spod1-ktisprod/cinder04",
     "management_url" => "http://admin:xw6RQkpwE4@10.4.15.243:2000/",
     "zone_name" => "cheonan.prod.ktis"
   }
