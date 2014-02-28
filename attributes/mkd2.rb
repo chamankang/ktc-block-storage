5e71eb1618476f6f3e57b76a865b6de549e9708f
Jesse Nelson <spheromak@gmail.com>
2014-02-28 01:03:27 -0800

    rubocopify

--- attributes/mkd2.rb
+++ attributes/mkd2.rb
@@ -1,60 +1,60 @@
-return unless chef_environment == "mkd2"
+return unless chef_environment == 'mkd2'
 
-include_attribute "ktc-block-storage::default"
+include_attribute 'ktc-block-storage::default'
 
-default["openstack"]["block-storage"]["enabled_backends"] = {
-  "nex-nfs1" => {
-    "volume_driver" => "cinder.volume.drivers.nexenta.nfs.NexentaNfsDriver"
+default['openstack']['block-storage']['enabled_backends'] = {
+  'nex-nfs1' => {
+    'volume_driver' => 'cinder.volume.drivers.nexenta.nfs.NexentaNfsDriver'
   }
 }
 
-default["openstack"]["block-storage"]["nexenta"]["nfs_shares"] = [
+default['openstack']['block-storage']['nexenta']['nfs_shares'] = [
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder1",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.ktis"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder1',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.ktis'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder2",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.ktis"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder2',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.ktis'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder3",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.ktis"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder3',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.ktis'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder4",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.ktis"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder4',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.ktis'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder5",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.dmz"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder5',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.dmz'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder6",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.dmz"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder6',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.dmz'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder7",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.dmz"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder7',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.dmz'
   },
   {
-    "server_ip" => "172.27.0.102",
-    "mount_point" => "/volumes/pool1/cinder8",
-    "management_url" => "http://admin:cloud123go@10.5.1.131:2000/",
-    "zone_name" => "mokdong.dev.dmz"
+    'server_ip' => '172.27.0.102',
+    'mount_point' => '/volumes/pool1/cinder8',
+    'management_url' => 'http://admin:cloud123go@10.5.1.131:2000/',
+    'zone_name' => 'mokdong.dev.dmz'
   }
 ]
